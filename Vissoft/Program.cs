5ba4e83fd478e56c40a10b8db7542beb5e3ca7f7
Hoang <nguyenviethoang2909@gmail.com>
2023-12-04 01:55:52 +0700

    update

--- Vissoft/Program.cs
+++ Vissoft/Program.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using Vissoft.Application.Services;
 using Vissoft.Core.Entities;
 using Vissoft.Core.Interfaces.IRepository;
 using Vissoft.Core.Interfaces.IService;
-using Vissoft.Extensions;
 using Vissoft.Helpers;
 using Vissoft.Infrastructure.Data;
 using Vissoft.Infrastructure.Identity;
@@ -22,6 +22,39 @@ builder.Services.AddDbContext<VissoftDatabaseContext>(options =>
 builder.Services.AddDbContext<IdentityDatabaseContext>(options =>
             options.UseMySql(connectionString,
             ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VissoftApi", Version = "v1" });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter JWT Bearer token",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    };
+    c.AddSecurityDefinition("Bearer", securityScheme);
+
+    var securityRequirement = new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    };
+    c.AddSecurityRequirement(securityRequirement);
+});
+
 
 builder.Services.AddAuthentication(options =>
 {
@@ -61,7 +94,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddControllers();
-builder.Services.AddApplicationServices();
 //services.AddIdentityService(Configuration);
 
 var app = builder.Build();
@@ -69,12 +101,11 @@ var app = builder.Build();
 //app.UseMiddleware<ExceptionMiddle>();
 //app.UseStatusCodePagesWithReExecute("/error/{0}");
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(option => option.SwaggerEndpoint("/swagger/v1/swagger.json", "VissoftJWToken v1"));
 app.UseHttpsRedirection();
-//app.UseSwaggerGen();
 app.UseRouting();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapControllers();
 app.Run();
